f625036f68d94acf11d7cc427f6b491064d6fe14
Diogo Soares <diogosoliv5@gmail.com>
2019-08-20 11:48:09 -0300

    exercicio 4 da ad1

--- exercicio4.py
+++ exercicio4.py
@@ -10,11 +10,23 @@ def converte(numDecimal, base):
     numConvertido = str(numDecimal) + numConvertido
     return numConvertido
 
+def converteDecimal(numBinario):
+    numConvertido = str(numBinario)
+    somaNum = 0
+    potencia = len(numConvertido)-1
+    for c in range(0, len(numConvertido)):
+        calc = int(numConvertido[c]) * (2 ** potencia)
+        potencia = potencia - 1
+        somaNum += calc
+
+    return somaNum
+
 
 # Programa Principal
 num = int(input())
 while num != -1:
+    numDecimal = converteDecimal(num)
     for b in range(3, 11):
-        print(converte(num, b), end=" ")
+        print(converte(numDecimal, b), end=" ")
     print()
     num = int(input())
